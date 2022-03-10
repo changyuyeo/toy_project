b42f640567436f11eb1dc859d7c5be8f694dab38
changyuyeo <wry5533@gmail.com>
2022-03-10 20:19:19 +0900

    clear project

--- src/lib/api/index.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const instance = (url: string) =>
-	axios.create({
-		baseURL: `${process.env.NEXT_PUBLIC_API_URL}/${url}`,
-		withCredentials: true
-	})
-
-export const user = instance('user')
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,9 @@
-import NextApp, { AppContext, AppProps } from 'next/app'
-import axios from 'axios'
-
-import Header from '@components/Header'
-import { wrapper } from '@store/index'
-import GlobalStyle from '@styles/GlobalStyled'
-import { cookieStringToObject } from '@utils/cookies'
-import { setLoggedUser } from '@store/user'
+import { AppProps } from 'next/app'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
 	<>
-		<GlobalStyle />
-		<Header />
 		<Component {...pageProps} />
-		<div id="root-modal" />
 	</>
 )
 
-MyApp.getInitialProps = wrapper.getInitialPageProps(store => async context => {
-	const cookieObject = cookieStringToObject(context.req?.headers.cookie)
-	console.log(context.req?.headers.cookie)
-	console.log(context.isServer)
-
-	const { user } = store.getState().user
-	// try {
-	// 	if (!isLogged && cookieObject.access_token) {
-	// 		axios.defaults.headers.cookie = cookieObject.access_token
-	// 		const { data } = await meAPI()
-	// 		store.dispatch(userActions.setLoggedUser(data))
-	// 	}
-	// } catch (e) {
-	// 	console.log(e.message)
-	// }
-	// return { ...appInitialProps })
-})
-
-export default wrapper.withRedux(MyApp)
+export default MyApp
